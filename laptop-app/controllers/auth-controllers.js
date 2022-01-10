86919aeebc69ea91c71c4a82abc85f45dd5a3a9c
affandiagung <ccoc.20001@gmail.com>
2022-01-11 02:57:32 +0700

    + selesai auth

--- laptop-app/controllers/auth-controllers.js
+++ laptop-app/controllers/auth-controllers.js
@@ -8,12 +8,12 @@ module.exports = {
     register :  async (req,res) => {
       const body =req.body //mengambil data2 body
       try {
-          const schema = Joi.object({  //validasi
+          const schema = Joi.object({  //validasi data yg dikirim dari body
             email : Joi.string().email().required(), //validasi berdasarkan email
             name : Joi.string().required(),
-            password : Joi.string().required()
+            password : Joi.string().min(6).required()
           })
-          const {error} = schema.validate(body) //jika valadsi gagal, error ada nilainya
+          const {error} = schema.validate(body) //jika validasi gagal, error ada nilainya
           if (error){
             return res.status(400).json({
               status : "Bad Request",
@@ -41,7 +41,6 @@ module.exports = {
             password : hashedPassword
           })
           
-          
           const token = jwt.sign({
             id : user.id,
             email : user.email
@@ -52,6 +51,53 @@ module.exports = {
             message : "Successfully save the data",
             result : token
           })
+      } catch (error) {
+        catchHandler (res,error)
+      }
+    },
+    login : async (req,res)=>{
+      const {email, password} = req.body
+      try {
+        const schema = Joi.object({ //validasi data yg dikirim
+          email : Joi.string().required(),
+          password : Joi.string().min(6).required()
+        })
+
+        const {error} = schema.validate({...req.body})
+        if (error){
+          res.status(400).json({
+            status : "Bad Request",
+            message : error.message
+          })
+        }
+        const user = await Users.findOne({ where :{ email }}) //mengambil data dari data base
+        
+        if(!user){
+          return res.status(201).json({
+            status :"Unauthorized",
+            message : "Invalid email or password",
+            result : {}
+          })
+        }
+        const isValid = await bcrypt.compare(password, user.password)
+        if (!isValid){
+          return res.status(201).json({
+            status :"Unauthorized",
+            message : "Invalid email or password",
+            result : {}
+          })
+        }
+        
+        const token = jwt.sign({
+          email : user.email,
+          id : user.id
+        },process.env.SECRET_TOKEN, {expiresIn : 60*60*12});
+
+        res.status(200).json({
+          status : "Success",
+          message : "Successfully login",
+          result : {token}
+        })
       } catch (error) {
         catchHandler (res,error)
       }
