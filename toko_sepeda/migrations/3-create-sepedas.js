38080261adb26023354a035f731b93fa52b4824d
affandiagung <ccoc.20001@gmail.com>
2022-01-12 16:47:14 +0700

    + membuat tugas

---
+++ toko_sepeda/migrations/3-create-sepedas.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Sepedas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      sepedaId:{
+        type : Sequelize.INTEGER,
+        allowNull : false,
+        references : {
+         model : "Sepedas",
+         key : "id"
+        },
+        onUpdate : "CASCADE",
+        onDelete : "CASCADE"
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      harga: {
+        type: Sequelize.INTEGER
+      },
+      stock: {
+        type: Sequelize.INTEGER
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Sepedas');
+  }
+};
